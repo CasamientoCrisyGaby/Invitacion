640eed3f1d5bac095094eb799b5c8c4e772bcbfd
CasamientoCrisyGaby <92033479+CasamientoCrisyGaby@users.noreply.github.com>
2022-10-12 17:10:18 -0300

    Delete audio.js

--- audio.js
+++
@@ -1,6 +0,0 @@
-<audio src='my.mp3'></audio>
-const audioContext = new AudioContext();
-const element = document.querySelector(audio);
-const source = audioContext.createMediaElementSource(element);
-source.connect(audioContext.destination)
-audio.play();
